dca580559b5e1081389bd4b3504ad4c6898c7214
Podbi <podbi@centrum.cz>
2017-01-11 11:24:07 +0100

    accounting: convert from old mobile

--- import/mobile.py
+++ import/mobile.py
@@ -9,54 +9,7 @@ CURRENCY_CZK = 1
 SOURCE_VALET = 1
 
 from service.repository import Repository
-
-class TypeResolver:
-    def __init__(self, database):
-        self.database = database
-        self.cursor = database.cursor()
-        self.types = {}
-        
-    def resolve(self, name):
-        types = self._getTypes()
-        name = self._formatTypeName(name)
-        
-        if name in types:
-            return types[name]
-        else:
-            raise Exception('Typ "{0}" není v databázi.'.format(name))
-    
-    def _getTypes(self):
-        if len(self.types) > 0:
-            return self.types
-        
-        self.cursor.execute('SELECT id, name FROM reports_recordtype ORDER BY id')
-        for type in self.cursor.fetchall():
-            self.types[self._formatTypeName(type[1])] = type[0]
-        
-        return self.types
-    
-    def _formatTypeName(self, name):
-        replacements = {
-            'ě' : 'e',
-            'š' : 's',
-            'č' : 'c',
-            'ř' : 'r',
-            'ž' : 'z',
-            'ý' : 'y',
-            'á' : 'a',
-            'í' : 'i',
-            'é' : 'e',
-            'ú' : 'u',
-            'ů' : 'u',
-        }
-        
-        name = name.lower()
-        for replace, replacement in replacements.items():
-            name = name.replace(replace, replacement)
-
-        return name
-    
-        
+from service.typeResolver import TypeResolver
 
 if len(sys.argv) < 3:
     raise Exception('Málo vstupních argumentů. Zadej název souboru a rok')
---
+++ import/old_mobile.py
@@ -0,0 +1,51 @@
+#  -*- coding: utf-8 -*-
+from datetime import datetime
+import sys
+import csv
+import sqlite3
+import re
+
+CURRENCY_CZK = 1
+SOURCE_VALET = 1
+
+if len(sys.argv) < 2:
+    raise Exception('Málo vstupních argumentů. Zadej název souboru.')
+
+date = ''
+records = []
+filepath = sys.argv[1]
+
+print('Soubor',filepath,'bude otevřen a zpracován')
+with open(filepath, 'r', encoding='utf-8', errors='replace') as file:
+    lines = file.readlines()
+print('Soubor byl úspěšně otevřen, zahajuji zpracovávání obsahu')
+for line in lines:
+    match = re.match(r'([0-9]+\.[0-9]+\.)?( )?(\-?[0-9]+)([ 0-9a-zA-Z,.]+)\(([ 0-9a-zA-Z,.]+)\)', line)
+    if match != None:
+
+        if match.group(1) != None:
+            date = match.group(1)
+            
+        if match.group(4) == None:
+            raise Exception('Řádek s popisem "{0}" nemá dostatek hodnot'.format(match.group(4)))
+        
+        print('.', end='')
+        
+        records.append([
+            date,
+            match.group(3),
+            match.group(4).strip(),
+            match.group(5),
+            None,
+        ])
+
+print('')
+print('Celkem bylo nalezeno',len(records),'záznamů, které budou exportovány do CSV souboru')
+
+with open('mobile.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Import'])
+    for record in records:
+        writer.writerow(record)
+        
+print('CSV soubor s názvem "mobile.csv" byl úspěšně vytvořen.')
