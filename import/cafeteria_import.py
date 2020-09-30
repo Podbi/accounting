f70a87143d09e77873f7ffea70bfabae3b06ec06
Podbi <podbi@centrum.cz>
2020-09-30 22:05:43 +0200

    [accounting] Benefit Plus converter added

---
+++ import/cafeteria_import.py
@@ -0,0 +1,108 @@
+#  -*- coding: utf-8 -*-
+from datetime import datetime
+import sys
+import csv
+import sqlite3
+import re
+
+CURRENCY_CZK = 1
+SOURCE_BANK = 2
+
+from service.repository import Repository
+from service.typeResolver import TypeResolver
+
+if len(sys.argv) < 2:
+    raise Exception('Málo vstupních argumentů. Zadej název souboru')
+
+date = ''
+records = []
+filepath = sys.argv[1]
+database = sqlite3.connect('../db.sqlite3')
+resolver = TypeResolver(database)
+repository = Repository(database)
+
+def formatDate(date):
+    match = re.search(r'([0-9]+)\.([0-9]+)\.([0-9]{4})', date)
+    if match != None:
+        date = datetime.strptime(match.group(3) + match.group(2) + match.group(1), '%Y%m%d')
+        return date.replace(hour=0, minute=0, second=0)
+    else:
+        match = re.search(r'([0-9]+)/([0-9]+)/([0-9]{4})', date)
+        if match != None:
+            date = datetime.strptime(match.group(3) + match.group(2) + match.group(1), '%Y%m%d')
+            return date.replace(hour=0, minute=0, second=0)
+    raise Exception('Datum "{0}" není ve správném formátu.'.format(date))
+
+def extractDescription(description):
+    match = re.search(r'(Food and Drink|Food and drink|Food)[ ]+(.+)', description)
+    if match != None:
+        return match.group(2)
+    return None
+
+def formatDescription(description):
+    extraction = extractDescription(description)
+    if extraction != None:
+        return 'Stravování (' + extraction + ')'
+    return ' '.join(description.split())
+
+def formatAmount(amount):
+    match = re.search(r'((-)?[0-9,]+\.[0-9]*)', amount)
+    if match != None:
+        return float(re.sub(r',', '', match.group(1)))
+    raise Exception('Částka "{0}" není ve správném formátu.'.format(amount))
+
+def formatPlace(description, place):
+    extraction = extractDescription(description)
+    if extraction != None:
+        return extraction
+    return place
+
+
+def resolveType(amount):
+    if amount > 0.0 :
+        return resolver.resolve('vyplata')
+    return resolver.resolve('stravovani')
+
+print('Soubor',filepath,'bude otevřen a zpracován')
+with open(filepath, 'r', encoding='utf-8', errors='replace') as file:
+    reader = csv.reader(file, delimiter=';')
+    print('Soubor byl úspěšně otevřen, zahajuji zpracovávání CSV')
+
+    next(reader)
+    for row in reader:
+        if len(row) < 5:
+            raise Exception('Řádek s popisem "{0}" nemá dostatek hodnot'.format(row[3]))
+
+        records.append({
+            'date' : formatDate(row[1]),
+            'money' : formatAmount(row[4]),
+            'currency' : CURRENCY_CZK,
+            'description' : formatDescription(row[2]),
+            'place' : formatPlace(row[2], row[5]),
+            'type' : resolveType(formatAmount(row[4])),
+            'source' : SOURCE_BANK
+        })
+
+print('')
+print('Celkem bylo nalezeno',len(records),'záznamů, které budou vloženy do databáze')
+print('Vkládám záznamy')
+counter = 0
+for record in records:
+    if (repository.hasRecord(record['date'], record['description'], record['money'], record['currency'], record['source'])):
+        print('Záznam ze dne', record['date'], 'za', record['money'], 'již existuje.')
+    else:
+        repository.createRecord(
+            record['date'],
+            record['description'],
+            record['place'],
+            record['money'],
+            record['currency'],
+            record['source'],
+            record['type']
+        )
+        counter = counter + 1
+        print('.', end='')
+        
+repository.commit()
+print('')
+print(counter,'záznamů bylo úspěšně vloženo')
